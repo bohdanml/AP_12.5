987b6e58d844d4c9096b9ec8e18d895bf4988646
Bohdan_PC <bogdaneuro235@gmail.com>
2024-04-30 21:07:22 +0300

    norm

--- AP_12.5/main.cpp
+++ AP_12.5/main.cpp
@@ -1,11 +1,52 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <stack>
 
 using namespace std;
 
+const int MAX_SIZE = 100; 
 
-void readNumbersFromFile(stack<int>& numberStack, const string& filename) {
+class Stack {
+private:
+    int elements[MAX_SIZE]; 
+    int top_index; 
+
+public:
+    Stack() : top_index(-1) {
+        
+        for (int i = 0; i < MAX_SIZE; ++i) {
+            elements[i] = 0;
+        }
+    } 
+    void push(int value) {
+        if (top_index == MAX_SIZE - 1) {
+            cerr << "Error: Stack is full." << endl;
+            return;
+        }
+        elements[++top_index] = value;
+    }
+
+    int pop() {
+        if (empty()) {
+            cerr << "Error: Stack is empty." << endl;
+            return -1;
+        }
+        return elements[top_index--];
+    }
+
+    bool empty() const {
+        return top_index == -1;
+    }
+
+    int top() const {
+        if (empty()) {
+            cerr << "Error: Stack is empty." << endl;
+            return -1;
+        }
+        return elements[top_index];
+    }
+};
+
+void readNumbersFromFile(Stack& numberStack, const string& filename) {
     ifstream file(filename);
     if (!file.is_open()) {
         cerr << "Error: Unable to open file " << filename << endl;
@@ -21,9 +62,8 @@ void readNumbersFromFile(stack<int>& numberStack, const string& filename) {
     file.close();
 }
 
-
-void printStack(const stack<int>& numberStack) {
-    stack<int> tempStack = numberStack;
+void printStack(const Stack& numberStack) {
+    Stack tempStack = numberStack;
     while (!tempStack.empty()) {
         cout << tempStack.top() << " ";
         tempStack.pop();
@@ -31,9 +71,8 @@ void printStack(const stack<int>& numberStack) {
     cout << endl;
 }
 
-
-void printNumbersInReverseOrderSkippingMultiples(const stack<int>& numberStack, int divisor) {
-    stack<int> tempStack = numberStack;
+void printNumbersInReverseOrderSkippingMultiples(const Stack& numberStack, int divisor) {
+    Stack tempStack = numberStack;
     while (!tempStack.empty()) {
         int num = tempStack.top();
         if (num % divisor != 0)
@@ -44,10 +83,24 @@ void printNumbersInReverseOrderSkippingMultiples(const stack<int>& numberStack, 
 }
 
 int main() {
-    stack<int> numberStack;
-    string filename = "numbers.txt"; 
+    Stack numberStack;
+    string filename = "numbers.txt";
 
-    readNumbersFromFile(numberStack, filename);
+    
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error: Unable to open file " << filename << endl;
+        return 1; 
+    }
+
+    
+    int num;
+    while (file >> num) {
+        if (num > 0)
+            numberStack.push(num);
+    }
+
+    file.close(); 
 
     cout << "Stack of numbers read from file:" << endl;
     printStack(numberStack);
@@ -57,7 +110,14 @@ int main() {
     cin >> divisor;
 
     cout << "Numbers of the stack in reverse order, skipping multiples of " << divisor << ":" << endl;
-    printNumbersInReverseOrderSkippingMultiples(numberStack, divisor);
+    Stack tempStack = numberStack;
+    while (!tempStack.empty()) {
+        int num = tempStack.top();
+        if (num % divisor != 0)
+            cout << num << " ";
+        tempStack.pop();
+    }
+    cout << endl;
 
     return 0;
 }
